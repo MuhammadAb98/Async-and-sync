4255b89a65cd4a0019792db38dad40315cb99be0
Muhammad Abdullah <56094500+MuhammadAb98@users.noreply.github.com>
2020-12-01 22:57:41 +0500

    1

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-console.log("Start Group");
-
-function breakfast(){
-setTimeout(function(){console.log("breakfast complete")},100);
+function printGreeting(msg, alertGreet) {
+    if(msg=='evening'){
+        evening(alertGreet);
+    }
+    else if(msg=='morning')
+    {
+        morning(alertGreet);
+    }
+    else
+    {
+        night(alertGreet);
+    }
 }
 
-function morning(){
-console.log("breakfast start");
-breakfast();
+function morning(alertGreet) {
+    alert("good morning");
+    alertGreet("Hello World");
+}
+function evening(alertGreet) {
+    alert("good evening");
+    alertGreet("Hello World");
+}
+function night(alertGreet) {
+    alert("good night");
+    alertGreet("Hello World");
 }
 
-morning(); 
-console.log("End Group");
+function alertGreet (msg) {
+alert(msg);
+}
 
+printGreeting("morning", alertGreet);
